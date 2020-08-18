2b81c3aea659cc8d76a58db9a30fb47d36eda0d8
simran279 <honey.simran279@gmail.com>
2020-08-19 00:15:48 +0530

    competitive programming

---
+++ backtracking/Nqueen.cpp
@@ -0,0 +1,68 @@
+/*
+You are given N, and for a given N x N chessboard,
+find a way to place N queens such that no queen can attack any other queen on the chess board.
+A queen can be killed when it lies in the same row, or same column, or the same diagonal of any of the other queens.
+You have to print all such configurations.
+Sample Input :
+4
+Sample Output :
+0 1 0 0 0 0 0 1 1 0 0 0 0 0 1 0
+0 0 1 0 1 0 0 0 0 0 0 1 0 1 0 0
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int board[11][11];
+
+bool isPossible(int n, int row, int col){ //since inserting row wise we check only in upper matrix
+    //same column
+    for(int i= row-1; i>= 0; i--){
+        if(board[i][col]== 1){
+            return false;
+        }
+    }
+    //upper left diagonal
+    for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--){
+        if(board[i][j] == 1){
+            return false;
+        }
+    }
+    //upper right diagonal
+    for(int i=row-1, j=col+1; i>=0 && j<n; i--, j++){
+        if(board[i][j] == 1){
+            return false;
+        }
+    }
+    return true;
+}
+void nQueenHelper(int n, int row){
+    if(row == n){
+       //reached solution print matrix
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                cout<<board[i][j]<< " ";
+            }
+        }
+        cout<<endl;
+        return;
+    }
+    //place at all possible position and move to smaller problem
+    for(int j=0; j<n; j++){
+        if(isPossible(n, row, j)){
+            board[row][j]= 1;
+            nQueenHelper(n, row+1);
+            board[row][j]= 0;
+        }
+    }
+    return;
+}
+void placeNQueens(int n){
+    memset(board, 0, 11*11*sizeof(int));
+    nQueenHelper(n, 0);
+}
+int main(){
+
+  int n;
+  cin >> n ;
+  placeNQueens(n);
+
+}
